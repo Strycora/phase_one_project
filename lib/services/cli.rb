f339dab2b7c98d5dcfb8cf64d01ff88e25d7576e
Vikram Chatterjee <strycora@gmail.com>
2020-12-01 18:25:51 -0700

    added spaces to improve aesthetics

--- lib/services/cli.rb
+++ lib/services/cli.rb
@@ -7,14 +7,17 @@ class Cli
     end
 
     def main_menu_options
-        puts "Enter 1 to get 10 random jokes!"
+        puts "  "
+        puts "Enter 1 in order to get 10 random jokes!"
         puts "Any other entry will exit the program."
+        puts "  "
         main_menu
 
     end
 
     def main_menu
-        input = get_input
+        input = get_input_one
+        puts "  "
         if input == "1"
            Jokes.all.each_with_index do |joke, index|
             puts "#{index+1}. #{joke.setup}"
@@ -22,18 +25,22 @@ class Cli
             end
             sub_menu
         else
-            return "Exiting program..."
+            puts "Exiting program..."
+            puts "  "
         end
     end
 
     def sub_menu
-        puts "Select a joke that you want to hear the punchline to"
-        input = get_input
+        puts "  "
+        puts "Select the number of the joke you want to hear the punchline to"
+        input = get_input_two
+        puts "  "
         if input == "1" || input == "2" || input == "3" || input == "4" || input == "5" || input == "6" || input == "7" || input == "8" || input == "9" || input == "10"
             index = input.to_i - 1 
                     joke = Jokes.all[index]
+                        puts "Punchline: "
                         puts "#{joke.punchline}"
-                        Jokes.clear
+                            Jokes.clear
         else 
             puts "Invalid Input"
             Jokes.clear
@@ -41,8 +48,14 @@ class Cli
         start
     end
 
-    def get_input
-        print "Enter Choice: "
+    def get_input_one
+        print "Enter Choice (1): "
         gets.chomp
     end
+
+    def get_input_two
+        print "Enter Choice (1-10): "
+        gets.chomp
+    end
+    
 end
